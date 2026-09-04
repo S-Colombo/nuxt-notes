639069a50b596442ca4789bf56c93bab445f9a48
S-Colombo <sergey.kalabin@legalbet.com>
2026-09-04 16:02:52 +0300

    fix styles

--- app/components/LayoutHeader.vue
+++ app/components/LayoutHeader.vue
@@ -28,6 +28,7 @@ import IconLogo from '~/components/icon/IconLogo.vue'
 .header {
   display: flex;
   width: 100%;
+  margin-top: 24px;
   padding: 0 16px;
   border-bottom: 1px solid var(--color-gray);
   @include breakpointMax(767px) {
@@ -36,12 +37,16 @@ import IconLogo from '~/components/icon/IconLogo.vue'
   }
   @include breakpointMin(768px) {
     justify-content: space-between;
-    align-items: flex-start;
-    margin: 48px auto 0;
+    align-items: center;
+    margin-right: auto;
+    margin-bottom: 0;
+    margin-left: auto;
   }
   &__brand {
     display: inline-flex;
-    padding-bottom: 24px;
+    @include breakpointMin(768px) {
+      padding-bottom: 24px;
+    }
   }
   &__menu {
     display: flex;
@@ -51,7 +56,7 @@ import IconLogo from '~/components/icon/IconLogo.vue'
       padding-bottom: 24px;
       border-bottom: 1px solid transparent;
       font-size: 16px;
-      line-height: 27px;
+      line-height: 50px;
       color: var(--color-black);
       text-decoration: none;
       transition: all 0.2s linear;
